36fb5609b723141ee75f20e96933f5167d5a5a3f
DELL <harshmalaviya@aviansoft.in>
2024-03-06 16:26:13 +0530

    CGPA 2

--- app/src/main/java/com/aviansoft/composedemo/MainActivity.kt
+++ app/src/main/java/com/aviansoft/composedemo/MainActivity.kt
@@ -1,39 +1,23 @@
 package com.aviansoft.composedemo
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.aviansoft.composedemo.ui.theme.ComposeDemoTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,20 +40,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CGPA() {
 
-    Column {
-        Text(text = "CGPA Calculator\nAapka Bhavishay")
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "CGPA Calculator\nAapka Bhavishay", modifier = Modifier.fillMaxWidth(), style = TextStyle(fontSize = 20.sp, textAlign = TextAlign.Center))
     }
 
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CGPAPreview() {
 
     ComposeDemoTheme {
+
         CGPA()
+
     }
-    
+
 }
 
