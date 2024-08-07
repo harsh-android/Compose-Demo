453af5386b534e8228ac56a844de3c6f633e579a
harshche123@gmail.com <harshche123@gmail.com>
2024-08-07 11:35:23 +0530

    Test 2

--- app/src/main/java/com/aviansoft/composedemo/MainActivity.kt
+++ app/src/main/java/com/aviansoft/composedemo/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.aviansoft.composedemo
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,7 +29,6 @@ import com.aviansoft.composedemo.ui.theme.ComposeDemoTheme
 
 
 // TODO This Demo is Not Fix working on IT
-
 
 
 class MainActivity : ComponentActivity() {
@@ -50,40 +52,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CGPA() {
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(
-            text = "CGPA Calculator\nAapka Bhavishay",
-            modifier = Modifier.fillMaxWidth(),
-            style = TextStyle(
-                fontSize = 20.sp,
-                fontStyle = FontStyle.Italic,
-                textAlign = TextAlign.Center,
-                color = Color(0xff000000)
-            )
-        )
-        Spacer(modifier = Modifier.padding(top = 10.dp))
-        subjectText("Subject 1")
+    Row(modifier = Modifier.fillMaxSize()) {
+
     }
 
 }
 
-
-
 @Composable
-fun subjectText(value: String) {
-    Text(
-        text = value,
-        modifier = Modifier.fillMaxWidth(),
-        style = TextStyle(
-            fontSize = 20.sp,
-            color = Color(0xff000000)
-        )
-    )
-}
-
-@Composable
-fun gradeText(grade:String) {
-    TextField(value = grade, onValueChange = {text->
+fun gradeText(grade: String) {
+    TextField(value = grade, onValueChange = { text ->
         onValueChange(text)
     })
 }
